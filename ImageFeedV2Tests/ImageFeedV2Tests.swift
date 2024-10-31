f52a0691c70d67c899f068e4087d8530665fa7af
Максим Карпов <maximmangk5@gmail.com>
2024-10-31 17:52:23 +0300

    Initial Commit

---
+++ ImageFeedV2Tests/ImageFeedV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedV2Tests.swift
+//  ImageFeedV2Tests
+//
+//  Created by Максим Карпов on 31.10.2024.
+//
+
+import Testing
+@testable import ImageFeedV2
+
+struct ImageFeedV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
