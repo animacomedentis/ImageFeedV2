aba3d6779a0a1b3b21c8195e2d61df6fa2c4932f
Максим Карпов <maximmangk5@gmail.com>
2024-11-05 16:43:35 +0300

    1. Helpers 2. Presentation 3. UIColor

---
+++ ImageFeedV2/Helpers/UIColor.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlack = UIColor(named: "YP Black") ?? UIColor.black
+    static var ypWhite = UIColor(named: "YP White") ?? UIColor.white
+    static var ypGray = UIColor(named: "YP Gray") ?? UIColor.gray
+    static var ypBlue = UIColor(named: "YP Blue") ?? UIColor.blue
+    static var ypRed = UIColor(named: "YP Red") ?? UIColor.red
+    static var ypBackground = UIColor(named: "YP Background") ?? UIColor.darkGray
+}
+
