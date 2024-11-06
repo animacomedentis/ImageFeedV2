9eb385bb6672face33d8fa5989382d8692711372
Максим Карпов <maximmangk5@gmail.com>
2024-11-06 18:34:40 +0300

    The cassette contains 20 photographs; there is a dent between the photographs; the edges of the photos are rounded; a label with the current date has been added to the photo; a “Like” button has been added to the photo; likes are given; sizes, parameters and indents of elements correspond to the layout; cell height depends on the photo size and is dynamic

--- ImageFeedV2/ImagesList/ImagesListCell.swift
+++ ImageFeedV2/ImagesList/ImagesListCell.swift
@@ -2,4 +2,21 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var date: UILabel!
+    
+    private var statuslike = false
+    
+    @IBAction func tapLikeButton(_ sender: Any) {
+        if statuslike == false {
+            likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
+            statuslike = true
+        }else {
+            likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
+            statuslike = false
+        }
+    }
 }
+
